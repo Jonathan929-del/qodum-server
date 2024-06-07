8decbd1f5e39ee749512bab27be3dea8d29ac0b5
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-06-07 16:40:25 +0300

    Changed installmeent fetching function to fetch all the installments

--- routes/installmentsRouter.js
+++ routes/installmentsRouter.js
@@ -18,44 +18,11 @@ router.get('/names', async (req, res) => {
     try {
 
         // Installments
-        const installments = await Installment.find({}, {name:1, due_on_date:1});
-
-
-        // Months map
-        const monthMapping = {
-            'January':0,
-            'February':1,
-            'March':2,
-            'April':3,
-            'May':4,
-            'June':5,
-            'July':6,
-            'August':7,
-            'September':8,
-            'October':9,
-            'November':10,
-            'December':11
-        };
-
-
-        // Is before today function
-        const isBeforeToday = dueDate => {
-            const {day, month, year} = dueDate;
-            const installmentDate = new Date(year, monthMapping[month], day);
-            const today = new Date();
-            today.setHours(0, 0, 0, 0); // Set the time of today to midnight to ignore the time part
-          
-            return installmentDate < today;
-        };
-
-
-        const beforeDueDateInstallments = installments.filter(installment => 
-            isBeforeToday(installment.due_on_date)
-        );
+        const installments = await Installment.find({}, {name:1});
 
 
         // Response
-        res.status(200).json(beforeDueDateInstallments);
+        res.status(200).json(installments);
 
     } catch (err) {
         res.status(500).json(err);
--- routes/paymentsRouter.js
+++ routes/paymentsRouter.js
@@ -1,4 +1,5 @@
 // Imports
+import crypto from 'crypto';
 import express from 'express';
 import Payment from '../models/Payment.js';
 
@@ -13,7 +14,99 @@ const router = express.Router();
 
 
 
-// Modify student fee heads
+// Generate hash function
+const generateHash = data => {
+    const hashString = `${data.key}|${data.txnid}|${data.amount}|${data.productinfo}|${data.firstname}|${data.email}|${data.udf1}|${data.udf2}|${data.udf3}|${data.udf4}|${data.udf5}|${data.udf6}|${data.udf7}|${data.udf8}|${data.udf9}|${data.udf10}|${process.env.EASEBUZZ_SALT}`;
+    return crypto.createHash('sha512').update(hashString).digest('hex');
+};
+
+
+
+
+
+// Send payment
+router.post('/payment/initiate-payment', async (req, res) => {
+    try {
+
+        // Request body
+        const {amount, purpose} = req.body;
+
+
+        // Prepare data for Easebuzz API
+        const txnid = Date.now().toString(); 
+        const hashData = {
+            key:process.env.EASEBUZZ_KEY,
+            txnid,
+            amount:amount.toString(),
+            firstname:'YourFirstName',
+            email:'youremail@example.com',
+            phone:'9999999999',
+            productinfo:purpose,
+            surl:'http://your-server-url/payments/payment/payment-success',
+            furl:'http://your-server-url/payments/payment/payment-failure',
+            udf1:'',
+            udf2:'',
+            udf3:'',
+            udf4:'',
+            udf5:'',
+            udf6:'',
+            udf7:'',
+            udf8:'',
+            udf9:'',
+            udf10:''
+        };
+        hashData.hash = generateHash(hashData);
+    
+
+        // Call Easebuzz API to create a payment request
+        const easebuzzResponse = await axios.post('https://sandbox.easebuzz.in/payments/initiate', hashData);
+    
+
+        // Reponse
+        res.json({payment_url:easebuzzResponse.data.payment_url});
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+// Payment success
+router.post('/payment/payment-success', async (req, res) => {
+    try {
+
+        // Reponse
+        res.send('Payment successful');
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+// Payment failure
+router.post('/payment/payment-failure', async (req, res) => {
+    try {
+
+        // Reponse
+        res.send('Payment failed');
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+// Create payment
 router.post('/payment/create', async (req, res) => {
     try {
 
