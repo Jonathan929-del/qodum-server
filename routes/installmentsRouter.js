ddc7ab3804d46f09752421ba9c24b0d445e4cfa7
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-06-14 12:36:13 +0300

    Created overdue installments fetcher

--- routes/installmentsRouter.js
+++ routes/installmentsRouter.js
@@ -33,5 +33,42 @@ router.get('/names', async (req, res) => {
 
 
 
+// Fetching late installments names
+router.get('/overdues', async (req, res) => {
+    try {
+
+        // All installments
+        const installments = await Installment.find({}, {name:1, due_on_date:1});
+        
+        
+        // Today's date
+        const today = new Date();
+    
+
+        // Convert today to comparable format
+        const currentDateString = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+        const currentDate = new Date(currentDateString);
+    
+        
+        // Filter installments where due_on_date is past today's date
+        const overdueInstallments = installments.filter(installment => {
+            const { day, month, year } = installment.due_on_date;
+            const dueDate = new Date(`${year}-${new Date(Date.parse(month +" 1, 2012")).getMonth() + 1}-${day}`);
+            return dueDate < currentDate;
+        });
+
+
+        // Response
+        res.status(200).json(overdueInstallments);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
 // Export
 export default router;
