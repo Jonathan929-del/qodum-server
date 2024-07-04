dd9ab43d6c35413c38e4169ff9053ee68b6ee8f1
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-07-04 17:41:36 +0300

    Fixed firebase issue

--- routes/notificationsRouter.js
+++ routes/notificationsRouter.js
@@ -1,10 +1,11 @@
 // Imports
+import fs from 'fs';
 import dotenv from 'dotenv';
 import cron from 'node-cron';
 import express from 'express';
-import {getFirestore, Timestamp} from 'firebase-admin/firestore';
+import admin from 'firebase-admin';
+import {getFirestore} from 'firebase-admin/firestore';
 import {getMessaging} from 'firebase-admin/messaging';
-import {initializeApp, cert} from 'firebase-admin/app';
 
 
 
@@ -19,23 +20,10 @@ dotenv.config();
 
 
 // Initializing app
-const serviceAccount = {
-    type:process.env.TYPE,
-    project_id:process.env.PROJECT_ID,
-    private_key_id:process.env.PRIVATE_KEY_ID,
-    private_key:process.env.PRIVATE_KEY,
-    client_email:process.env.CLIENT_EMAIL,
-    client_id:process.env.CLIENT_ID,
-    auth_uri:process.env.AUTH_URI,
-    token_uri:process.env.TOKEN_URI,
-    auth_provider_x509_cert_url:process.env.AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url:process.env.CLIENT_CERT_URL,
-    universe_domain:process.env.UNIVERSE_DOMAIN
-  }
-initializeApp({
-    credential:cert(serviceAccount),
+admin.initializeApp({
+    credential:admin.credential.cert(JSON.parse(fs.readFileSync('./qodum-9c179-firebase-adminsdk-zylgw-84904d0ba2.json', 'utf8'))),
 });
-const db = getFirestore();
+const db = admin.firestore();
 
 
 
