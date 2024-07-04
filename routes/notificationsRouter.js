a1322acdd2f4965acb45e8c2c97ef2d447c0343b
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-07-05 00:59:36 +0300

    Commit

--- routes/notificationsRouter.js
+++ routes/notificationsRouter.js
@@ -22,17 +22,17 @@ dotenv.config();
 admin.initializeApp({
     // credential:admin.credential.cert(JSON.parse(fs.readFileSync('./qodum-9c179-firebase-adminsdk-zylgw-84904d0ba2.json', 'utf8')))
     credential:admin.credential.cert({
-        "type":process.env.TYPE,
-        "project_id":process.env.PROJECT_ID,
-        "private_key_id":process.env.PRIVATE_KEY_ID,
-        "private_key":process.env.PRIVATE_KEY,
-        "client_email":process.env.CLIENT_EMAIL,
-        "client_id":process.env.CLIENT_ID,
-        "auth_uri":process.env.AUTH_URI,
-        "token_uri":process.env.TOKEN_URI,
-        "auth_provider_x509_cert_url":process.env.AUTH_PROVIDER_CERT_URL,
-        "client_x509_cert_url":process.env.CLIENT_CERT_URL,
-        "universe_domain":process.env.UNIVERSE_DOMAIN
+        type:process.env.TYPE,
+        project_id:process.env.PROJECT_ID,
+        private_key_id:process.env.PRIVATE_KEY_ID,
+        private_key:process.env.PRIVATE_KEY,
+        client_email:process.env.CLIENT_EMAIL,
+        client_id:process.env.CLIENT_ID,
+        auth_uri:process.env.AUTH_URI,
+        token_uri:process.env.TOKEN_URI,
+        auth_provider_x509_cert_url:process.env.AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url:process.env.CLIENT_CERT_URL,
+        universe_domain:process.env.UNIVERSE_DOMAIN
     })
 });
 const db = admin.firestore();
